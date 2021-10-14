f298531171618dec1feaf2889b7b3758504d3f9f
Jatin17Solanki <jatin.solanki1@learner.manipal.edu>
2021-10-14 16:16:52 +0530

    stage 2 buggg

--- components/user/User.jsx
+++ components/user/User.jsx
@@ -30,25 +30,39 @@ const info = [
 const fetchUserDetails = async (userWallet) => {
   const url = `/api/user/?walletAddress=${userWallet}`;
 
-  const details = await fetch(url);
+  const response = await fetch(url);
+  const details = await response.json();
 
-  console.log(await details.json());
+  return details.data;
 };
 
 const User = () => {
   const [userWallet, setUserWallet] = React.useState("");
+  const [regNo, setRegNo] = React.useState("");
+  const [userName, setUserName] = React.useState("");
+
   React.useEffect(() => {
     web3.eth.getAccounts().then((res) => {
       const displayAddress = res[0].slice(0, 6) + ".." + res[0].slice(-5);
       setUserWallet(displayAddress);
-      fetchUserDetails(res[0]);
+      fetchUtil(userWallet);
     });
+
+    const fetchUtil = async(wallet) => {
+      console.log(wallet);
+      const details = await fetchUserDetails(wallet);
+      console.log(details);
+      setRegNo(details.regNo);
+      setUserName(details.name);
+    }
+
+    
   }, []);
 
   return (
     <React.Fragment>
-      <h1 className="text-3xl">Yash Aryan, {userWallet}</h1>
-      <p>180934136</p>
+      <h1 className="text-3xl">{userName}</h1>
+      <p>{regNo}</p>
       <div className="mt-10">
         {info.map((item) => (
           <CertificateComponent {...item} />
