15da1af6d5156a4c192a583649e7c89265a7c0ea
Yash Aryan <yasharyan307@outlook.com>
2021-10-17 16:20:03 +0530

    UI Improvements

--- components/admin/CertificateCards.jsx
+++ components/admin/CertificateCards.jsx
@@ -27,17 +27,20 @@ const revoke = async (
       const response = await deleteCertificate.json();
       if (response.success === true) {
         alert("Certificated successfully deleted and revoked!!");
+        closeLoading();
         refreshFunction();
       } else {
         alert("Could not delete from db!");
+        closeLoading();
       }
     } else {
       alert("Could not revoke Certificate!");
+      closeLoading();
     }
   } catch (err) {
     alert("DD: " + err.message);
+    closeLoading();
   }
-  closeLoading();
 };
 
 const CertificateCards = (props) => {
@@ -84,10 +87,10 @@ const CertificateCards = (props) => {
           </button>
         ) : (
           <div
-            className={`px-5 py-1 text-white ${styles.button} h-12 w-1/3 bg-red-400`}
+            className={`px-5 py-1 text-white ${styles.button} h-12 w-1/3 bg-red-400 flex flex-col justify-center`}
           >
             <div
-              className={`${homestyles.loader_red} ease-linear rounded-full border-4 border-t-4 border-white h-8 w-8 mx-auto`}
+              className={`${homestyles.loader_red} ease-linear rounded-full border-4 border-t-4 border-white h-6 w-6 mx-auto mt-2`}
             ></div>
           </div>
         )}
